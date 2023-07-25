fc1fb95b9ca6aa785df5bbe0ee21a2749bb5de97
Donald Twiford <donaldptwiford@gmail.com>
2023-07-24 20:05:46 -0700

    update sql file

--- association-sql.sql
+++ association-sql.sql
@@ -2,14 +2,14 @@ CREATE TABLE unit_category (
 	category_id text NOT NULL,
 	PRIMARY KEY (category_id),
 	category_name text NOT NULL
-)
+);
 
 CREATE TABLE image (
 	image_id int GENERATED ALWAYS AS IDENTITY,
 	PRIMARY KEY (image_id),
 	image_name text NOT NULL,
 	image_link text NOT NULL
-)
+);
 
 CREATE TABLE bracket (
 	unit text NOT NULL,
@@ -18,7 +18,7 @@ CREATE TABLE bracket (
 	FOREIGN KEY (category) REFERENCES unit_category(category_id),
 	range_min int NOT NULL,
 	range_max int NOT NULL
-)
+);
 
 CREATE TABLE image_bracket (
 	_id int GENERATED ALWAYS AS IDENTITY,
@@ -27,4 +27,7 @@ CREATE TABLE image_bracket (
 	FOREIGN KEY (image_FK) REFERENCES image(image_id),
 	backet_FK text NOT NULL,
 	FOREIGN KEY (backet_FK) REFERENCES bracket(unit)	
-)
+);
+
+ALTER TABLE image
+ADD grams int;

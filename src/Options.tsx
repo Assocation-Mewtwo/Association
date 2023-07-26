b29f705f201e78b3e867d13bcfa643703551a985
missmshel <mshelconroy@gmail.com>
2023-07-26 14:03:29 -0600

    merges file changes to ComparedTo.tsx and creation of Options.tsx from michelle/comparisons branch with the dev branch. co-author: cronullarc
    ryancronulla@gmail.com
    
    co-author: missmshel
    
    mshelconroy@gmail.com
    
    co-author: KrankyKnight
    
    donaldptwiford@gmail.com
    
    co-author: garrettallen0
    
    Garrett.allen13@gmail.com

---
+++ src/Options.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+type optionsProps = {
+  id: number;
+  tbdFunction: (event: any) => void;
+  compareOptions: object[];
+};
+
+const Options = ({ id, compareOptions }: optionsProps) => {
+  return (
+    // need to build out tbdFunction
+    <>
+      <option key={`option${id}`} value={`${id}`}>
+        {compareOptions[id].name_single}
+      </option>
+    </>
+  );
+};
+
+export default Options;
